5a708918cd67aff76d8b06e06475aa979c9ce979
Sam Park <samuel.d.g.park@gmail.com>
2018-02-11 09:16:58 -0800

    adjust config

--- config/config.js
+++ config/config.js
@@ -4,14 +4,8 @@ module.exports = (() => {
       port: process.env.PORT || 8087,
       env: process.env.NODE_ENV || 'dev',
       logLevel: process.env.LOG_LEVEL || 'debug',
-      tokenSecret: process.env.TOKEN_SECRET || 'asdf'
-    },
-    external: {
-      spotify: {
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        tokenEndpoint: process.env.SPOTIFY_AUTH_ENDPOINT,
-      }
+      tokenSecret: process.env.TOKEN_SECRET || 'asdf',
+      dbConnection: process.env.MONGO_URI
     }
   };
 
