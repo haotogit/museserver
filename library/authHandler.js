ac981086f9a57cdb515b7dccb9e5860b0a2b56ff
Sam Park <samuel.d.g.park@gmail.com>
2018-02-16 23:37:31 -0800

    fix auth handler

--- library/authHandler.js
+++ library/authHandler.js
@@ -1,6 +1,10 @@
 const tokenCheck = require('./tokenCheck');
 
 module.exports = (req, res, next) => {
+  let whiteList = /healthcheck|(users\/auth)|authSpotify/;
+
+  if (whiteList.test(req.url) || (/users/.test(req.url) && req.method === 'POST')) return next();
+
   let token = req.headers && req.headers.authorization ? req.headers.authorization : null;
 
   tokenCheck(token)
