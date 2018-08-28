b5f9b49271e0555e97d2819db08f57a853ba7284
Sam Park <samuel.d.g.park@gmail.com>
2018-08-27 18:07:27 -0700

    checking token at auth handler

--- library/authHandler.js
+++ library/authHandler.js
@@ -1,10 +1,11 @@
 const tokenCheck = require('./tokenCheck');
 
 module.exports = (req, res, next) => {
-  next();
-  //let token = req.headers && req.headers.authorization ? req.headers.authorization : null;
+  let token = req.headers && req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
 
-  //tokenCheck(token)
-  //  .then(result => next())
-  //  .catch(err => res.status(err.statusCode || 401).send(err.message || 'Error with token validation'));
+  tokenCheck(token)
+    .then(result => {
+      next()
+    })
+    .catch(err => res.status(err.statusCode || 401).send(err.message || 'Error with token validation'));
 };
